890500718492d3f22f7ab46761c50c96f8d2e59b
unknown <crunchydeer30@gmail.com>
2023-03-09 21:08:41 +0300

    Completed project 6

---
+++ Project 6 - Scroll Animation/script.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', showBoxes);
+
+showBoxes();
+
+function showBoxes() {
+    const breakpoint = window.innerHeight * 0.9;
+    
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < breakpoint) {
+            box.classList.add('show');
+        }
+        else {
+            box.classList.remove('show');
+        }
+    })
+    
+}
