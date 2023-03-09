4cb32d675036dab584bbbf8a7e412d1196cc0fb0
unknown <crunchydeer30@gmail.com>
2023-03-09 18:48:55 +0300

    Fixed projects 6 script bug

--- Project 6 - Scroll Animation/script.js
+++ Project 6 - Scroll Animation/script.js
@@ -2,6 +2,7 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', showBoxes);
 
+showBoxes();
 
 function showBoxes() {
     const breakpoint = window.innerHeight * 0.9;
